e1c1f6b2c7e9ade30cc9ef879045a42378235e9c
2blam <cshwlam@gmail.com>
2017-07-28 17:15:31 +0800

    Create quicksort.py

---
+++ sorting/quicksort.py
@@ -0,0 +1,19 @@
+def quicksort(lst):
+    if len(lst) <=1:
+        return lst
+    less, equal, greater = [], [], []
+    # pick the first element as pivot (can use last one / random element)
+    pivot = lst[0]
+    
+    for e in lst:
+        if e < pivot:
+            less.append(e)
+        elif e > pivot:
+            greater.append(e)
+        else:
+            equal.append(e)
+    less = quicksort(less)
+    greater = quicksort(greater)
+    return less + equal + greater            
+    
+print(quicksort([3, 5, 4, 6, 12, 8, 2, 1]))        
